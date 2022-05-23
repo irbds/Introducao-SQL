b9c32e07602aff0383bd2602f1ff6e222718ef89
igor <igor.rafael.briano@gmail.com>
2022-05-23 20:43:17 -0300

    excluido arquivos sql pois eles existem agora em uma branch para melhor organizar os exercicios dos proximos dias

--- Exer-3.sql
+++
@@ -1,5 +0,0 @@
-# Em seguida, faça uma consulta para exibir as peças, preço e fornecedor de todos os 
-# fornecimentos em que o código do fornecedor tenha a letra N.
-USE PecasFornecedores;
-SELECT peca, Preco, Fornecedor FROM Fornecimentos
-WHERE Fornecedor LIKE '%N%'
--- Exer-4.sql
+++
@@ -1,6 +0,0 @@
-# Avante, falta pouco! Escreva uma query para exibir todas as informações dos fornecedores que 
-# são empresas limitadas (LTDA). Ordene esses resultados em ordem alfabética decrescente.
-USE PecasFornecedores;
-SELECT * FROM Fornecedores
-WHERE name LIKE '%LTDA'
-ORDER BY name
