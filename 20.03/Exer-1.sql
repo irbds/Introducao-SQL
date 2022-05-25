dd732c15d55be4ff3fa418b9c75fbc40234326b6
igor <igor.rafael.briano@gmail.com>
2022-05-24 21:04:01 -0300

    desfazendo as alteracoes feitas para o merge

--- 20.03/Exer-1.sql
+++
@@ -1,4 +0,0 @@
-# Vamos lá! Faça uma consulta que retorne todas as peças que começam com as letras GR.
-USE PecasFornecedores;
-SELECT * FROM Pecas
-WHERE name LIKE 'Gr%'
--- 20.03/Exer-2.sql
+++
@@ -1,6 +0,0 @@
-# Agora, escreva uma query para mostrar todos os fornecimentos que contenham a peça com code 2. 
-# Organize o resultado por ordem alfabética de fornecedor.
-USE PecasFornecedores;
-SELECT * FROM Fornecimentos
-WHERE peca = 2
-ORDER BY Fornecedor 
